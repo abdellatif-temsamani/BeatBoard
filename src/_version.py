4ea8039338dd4ea20cb23a578c0cf0337d3c5cff
abdellatif Temsamani <contact@abdellatif.dev>
2025-12-06 20:54:08 +0100

    style: format src/_version.py with ruff
    - Change single quotes to double quotes for consistency

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev15+g8910d1aff.d20251206'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev15', 'g8910d1aff.d20251206')
+__version__ = version = "0.1.1.dev15+g8910d1aff.d20251206"
+__version_tuple__ = version_tuple = (0, 1, 1, "dev15", "g8910d1aff.d20251206")
 
-__commit_id__ = commit_id = 'g8910d1aff'
+__commit_id__ = commit_id = "g8910d1aff"
